f18c9581cd306d53ab8a90cc0c07084b9fd8fbe4
Bekhzod Umarov <theimpaler@legend>
2016-12-03 12:57:11 -0500

    Town indecies

--- main/views.py
+++ main/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import pandas as pd
+
+
+def get_town_index():
+    with open('TOWN_INDEX.geojson', 'r') as f:
+        towns = f.read();
+    return towns
 
 # Create your views here.
 def index(request):
-    context = {'name':'user'}
+    context = {
+            'name':'user', 
+            'towns':get_town_index()
+    }
+
     return render(request, 'main/index.html', {'data':context})
